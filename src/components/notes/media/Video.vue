0fc7f5b9ac9950db9a621ebd4fcac70cc6655927
superjoefly <superjoefly@gmail.com>
2017-12-19 00:22:28 -0700

    Added title for page.

--- src/components/notes/media/Video.vue
+++ src/components/notes/media/Video.vue
@@ -3,9 +3,11 @@
 
     <div style="width: 500px; max-width: 90vw">
 
+      <p class="text-bold">Video:</p>
+
       <q-video src="https://www.youtube.com/embed/s87EppmsJv0?rel=0" />
 
-      <p class="text-bold">With HTML Markup</p>
+      <p class="text-bold">Using HTML Markup:</p>
 
       <div class="q-video">
         <iframe src="https://www.youtube.com/embed/s87EppmsJv0" frameborder="0" allowfullscreen></iframe>
